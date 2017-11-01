9f0bf1679e04a63c7a5e11a8db4cf32717384473
apriltanpaoyin <d14124009@mydit.ie>
2017-11-01 18:16:21 +0000

    CLAHE, .avi

--- Assignment 2/kernel.py
+++ Assignment 2/kernel.py
@@ -18,10 +18,12 @@
 #	1. Stackoverflow.com. (2014). Python "SyntaxError: Non-ASCII character '\xe2' in file". [online] Available at: https://stackoverflow.com/questions/21639275/python-syntaxerror-non-ascii-character-xe2-in-file [Accessed 4 Oct. 2017].																			
 #
 #	Performance:																																																								
-#	1. 																													
+#	1. Suuuuuuper slow cause of denoising																									
 #
 #	Experiments:																							
-#	1. To include links in this file, the first line had to be implemented. This solution was found online. Please see Reference #1.																											
+#	1. To include links in this file, the first line had to be implemented. This solution was found online. Please see Reference #1.
+#	2. Tried to use the input file type as the output file type, but it could not find the library (for mp4 in the case of Zorro). 
+#		Decided to scrap to prevent	compatibility issues. 																							
 ################################################################################################################################################################################################################################################
 # Import the necessary packages:
 import numpy as np
@@ -30,13 +32,52 @@ import cv2
 video = cv2.VideoCapture("Zorro.mp4")
 # Params needed for video writer
 # fourcc = int(video.get(cv2.CAP_PROP_FOURCC))
-fourcc = cv2.VideoWriter_fourcc(*"MPEG")
+# fourcc = cv2.VideoWriter_fourcc(*'MP4V')
 fps = int(video.get(cv2.CAP_PROP_FPS))
 frameSize = (int(video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
 # Define video writer to writea new video
-writer = cv2.VideoWriter("output.mp4", fourcc, fps, frameSize)
+# writer = cv2.VideoWriter("output.avi", fourcc, fps, frameSize)
+writer = cv2.VideoWriter("output.avi", -1, fps, frameSize)
 cnt = 0
+
+def processFrame(cnt, img):
+	# if cnt == 150:
+	# 	cv2.imwrite("ori_150.jpg", img)
+	# if cnt == 600:
+	# 	cv2.imwrite("ori_600.jpg", img)
+
+	# Add edges to the image to make the edges sharper?
+
+	# if cnt == 650:
+	denoise = cv2.fastNlMeansDenoising(img,None,7,7,21)
+
+	denoise = cv2.cvtColor(denoise, cv2.COLOR_BGR2GRAY)
+
+	clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(2,2))
+	cla = clahe.apply(denoise)
+
+	# Sharpen
+	kernel = np.array([[0,-1,0], [-1,5,-1], [0,-1,0]], dtype=float)
+	kernImg = cv2.filter2D(denoise,ddepth=-1,kernel=kernel)
+
+	# Gaussian from notes
+	kernel = np.array([[1,4,1], [4,7,4], [1,4,1]], dtype=float)/27
+	smooth = cv2.filter2D(kernImg,ddepth=-1,kernel=kernel)
+
+		# cv2.imshow("cla", cla)
+		# cv2.imshow("smooth", smooth)
+		# key = cv2.waitKey(0)
+
+		# cv2.imwrite("mod_{}.jpg".format(cnt), smooth)
+	# if cnt == 600:
+	# 	denoise = cv2.fastNlMeansDenoising(img,None,7,7,21)
+
+	# 	kernel = np.array([[0,-1,0], [-1,5,-1], [0,-1,0]], dtype=float)
+	# 	kernImg = cv2.filter2D(denoise,ddepth=-1,kernel=kernel)
+
+	# 	cv2.imwrite("mod_600.jpg", kernImg)
+	return smooth
 
 while True:
 	(grabbed, img) = video.read()
@@ -45,33 +86,12 @@ while True:
 	#if can't grab frame
 	if not grabbed:
 		break
+	else:
+		smooth = processFrame(cnt, img)
+		print cnt
+		writer.write(smooth)
 
-	# if cnt == 150:
-	# 	cv2.imwrite("ori_150.jpg", img)
-	# if cnt == 600:
-	# 	cv2.imwrite("ori_600.jpg", img)
 	
-	# kernel = np.array([[-1,-1,-1], [-1,8,-1], [-1,-1,-1]], dtype=float)
-	# kernImg = cv2.filter2D(img,ddepth=-1,kernel=kernel)
-
-	# Add edges to the image to make the edges sharper?
-
-	# if cnt > 140 and cnt < 150:
-	denoise = cv2.fastNlMeansDenoising(img,None,7,7,21)
-
-	kernel = np.array([[0,-1,0], [-1,5,-1], [0,-1,0]], dtype=float)
-	kernImg = cv2.filter2D(denoise,ddepth=-1,kernel=kernel)
-
-		# cv2.imwrite("mod_150.jpg", kernImg)
-	# if cnt == 600:
-	# 	denoise = cv2.fastNlMeansDenoising(img,None,7,7,21)
-
-	# 	kernel = np.array([[0,-1,0], [-1,5,-1], [0,-1,0]], dtype=float)
-	# 	kernImg = cv2.filter2D(denoise,ddepth=-1,kernel=kernel)
-
-	# 	cv2.imwrite("mod_600.jpg", kernImg)
-	writer.write(kernImg)
-
 video.release()
-writer.release()
+# writer.release()
 cv2.destroyAllWindows()
